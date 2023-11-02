6f1bb0cf75b47d1a75aa762f81233457a1766cf1
unknown <sukmaaji11@gmail.com>
2023-11-02 13:29:12 +0700

    Create SnapVideo Development

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1 className='text-3xl font-bold underline'>
+        Hai Bro!
+     </h1>
+    </div>
+  );
+}
+
+export default App;
